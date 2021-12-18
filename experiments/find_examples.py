f3630dff6584ac27b2c92e3fc88cea71cf0ca041
Tim Fischer <tim.s.fischer96@googlemail.com>
2021-12-18 12:34:38 +0100

    small fix

--- experiments/find_examples.py
+++ experiments/find_examples.py
@@ -3,9 +3,12 @@ from pathlib import Path
 import numpy as np
 import torch
 
-in_file = Path("./summeval/bertscore/doc.json")
+dataset = "xsum"
+metric = "entailment"
+variant = "sent"
+in_file = Path(f"{dataset}/{metric}/{metric}_{variant}.json")
 max_num_sentences = 30
-score_key = "doc_precision"  # or recall or f1
+score_key = "precision"  # or recall or f1
 
 with in_file.open(mode="r", encoding="UTF-8") as file:
     data = json.load(file)
@@ -24,12 +27,16 @@ difference = torch.tensor(np.abs(scores - faithfulness))
 good_example_ids = torch.topk(difference, k=100, largest=False).indices.tolist()
 bad_example_ids = torch.topk(difference, k=100, largest=True).indices.tolist()
 
+# dont take the 50 worst or 50 best
+good_example_ids = good_example_ids[50:]
+bad_example_ids = bad_example_ids[50:]
+
 good_examples = [data[idx] for idx in good_example_ids]
 bad_examples = [data[idx] for idx in bad_example_ids]
 
 # Write data
-out_file_good = in_file.with_name(in_file.name.replace(".json", "_good_examples.json"))
-out_file_bad = in_file.with_name(in_file.name.replace(".json", "_bad_examples.json"))
+out_file_good = in_file.with_name(in_file.name.replace(".json", "_examples_good.json"))
+out_file_bad = in_file.with_name(in_file.name.replace(".json", "_examples_bad.json"))
 with out_file_good.open(encoding="UTF-8", mode="w") as file:
     json.dump(good_examples, file)
 with out_file_bad.open(encoding="UTF-8", mode="w") as file:
--- experiments/visualize_all_metrics.py
+++ experiments/visualize_all_metrics.py
@@ -8,23 +8,24 @@ from faithfulness.SRL import SRL
 from faithfulness.SentSim import SentSim
 from faithfulness.visualization.Visualizer import Visualizer
 
-input_file = Path("./xsum/bertscore/bertscore_doc.json")
-Visualizer(file_path=input_file, metric=BERTScore).visualize()
+for variant in ["good", "bad"]:
+    input_file = Path(f"./xsum/analysis/bertscore_sent_examples_{variant}.json")
+    Visualizer(file_path=input_file, metric=BERTScore).visualize()
 
-input_file = Path("./xsum/entailment/entailment_sent.json")
-Visualizer(file_path=input_file, metric=Entailment).visualize()
+    input_file = Path(f"./xsum/analysis/entailment_sent_examples_{variant}.json")
+    Visualizer(file_path=input_file, metric=Entailment).visualize()
 
-input_file = Path("./xsum/ner/ner_em.json")
-Visualizer(file_path=input_file, metric=NER).visualize()
+    input_file = Path(f"./xsum/analysis/ner_em_examples_{variant}.json")
+    Visualizer(file_path=input_file, metric=NER).visualize()
 
-input_file = Path("./xsum/openie/openie_em.json")
-Visualizer(file_path=input_file, metric=OpenIE).visualize()
+    input_file = Path(f"./xsum/analysis/openie_f1_examples_{variant}.json")
+    Visualizer(file_path=input_file, metric=OpenIE).visualize()
 
-input_file = Path("./xsum/qgqa/qgqa_em.json")
-Visualizer(file_path=input_file, metric=QGQA).visualize()
+    input_file = Path(f"./xsum/analysis/qgqa_f1_examples_{variant}.json")
+    Visualizer(file_path=input_file, metric=QGQA).visualize()
 
-input_file = Path("./xsum/sentsim/sentsim_em.json")
-Visualizer(file_path=input_file, metric=SentSim).visualize()
+    input_file = Path(f"./xsum/analysis/sentsim_f1_examples_{variant}.json")
+    Visualizer(file_path=input_file, metric=SentSim).visualize()
 
-input_file = Path("./xsum/srl/srl_em.json")
-Visualizer(file_path=input_file, metric=SRL).visualize()
+    input_file = Path(f"./xsum/analysis/srl_grouped_sentcos_examples_{variant}.json")
+    Visualizer(file_path=input_file, metric=SRL).visualize()
